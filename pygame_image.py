e7c7fe31fd2b1c493b8153310a6716f844c5d382
c0a2418277 <c0a2418277@edu.teu.ac.jp>
2025-04-15 17:41:13 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -24,15 +24,20 @@ def main():
         # print(key_lst, [pg.K_UP])
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0,-1))
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1,0))
-        else:
-            kk_rct.move_ip((-1,0))
-        
+        elif key_lst[pg.K_DOWN]:
+            h=0
+            v=1            
+        elif key_lst[pg.K_LEFT]:
+            h=-2
+            v=0
+        elif key_lst[pg.K_RIGHT]:
+            h=1
+            v=0
+                
+        elif not key_lst[pg.K_RIGHT]:
+            h=-1
+            v=0
+        kk_rct.move_ip((h,v))
         x = tmr%3200
         
         screen.blit(bg_img, [-x, 0])
